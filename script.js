bbab017a5bd1c4335bc40419f9fd2f83c7d7581a
Brandon <brandon.silva.180@gmail.com>
2025-04-17 12:22:42 -0600

    Primer commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
